9e206761534a5083842ba4bd0b44d1eaa03788a7
tranthoai142 <tranthoai142@gmail.com>
2020-01-03 21:06:15 +0700

    create layout basic, menu navbar footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
